8e61eb1fffe3fe1e1c0b3ce1b934286e21f6a99b
Shuwani <68225758+Shuwani@users.noreply.github.com>
2021-09-29 21:22:27 +0530

    Update App.js

--- LGM TASK 2/src/App.js
+++ LGM TASK 2/src/App.js
@@ -1,85 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Fetch2 from './Hooks/Fetch2';
-// import UseStateArray from './Hooks/UseStateArray';
-// import UseStatewithObject from './Hooks/UseStatewithObject';
-// import UseStateArray from './Hooks/UseStateArray';
-// import UseStatewithPrevState from './Hooks/UseStatewithPrevState';
-// import Post from './Component/Post';
-// import Get from './Component/Get';
-// import Counter from './Component/Counter';
-// import RefsInClass from './RefsInClass';
-// import Refs from './Component/Refs';
-// import Form from './Form/Refs';
-// import FetchAPI frosm './Component/FetchAPI';
-// import Stylesheet from './Component/Stylesheet'
-// import List from './Component/List';
-// import Condition from './Component/Condition';
-// import ParentComp from './Component/ParentComp';
-// import States from './Component/States'
-// import Setstate from './Component/Setstate'
-// import Welcome from './Component/Welcome'
-// import Prop from './Component/Prop'
-// import Eventhandling from './Component/Eventhandling';
-
 
 function App() {
-
-// const App=()=>{
-
+  
   return (
     <div className='App'>
       <Fetch2/>
-
-      {/* <UseStateArray/> */}
-      
-      {/* <UseStatewithObject/> */}
-      {/* <UseStatewithPrevState/> */}
-
-      {/* <Post/> */}
-      {/* <Get/> */}
-
-      {/* <Counter/> */}
-      {/* <RefsInClass/> */}
-
-      {/* <Refs/> */}
-
-      {/* <Form/> */}
-
-    {/* <FetchAPI/> */}
-
-      {/* <Stylesheet/> */}
-
-      {/* <List/> */}
-
-      {/* <Condition/> */}
-      
-      {/* <ParentComp/> */}
-      
-      {/* <setstate/> */}
-
-
-      {/* <Prop name="SHUWANI">
-      <p>This is my daughter</p>
-    </Prop>
-    <Prop name="SHUHASHI">
-      <button>Action</button>
-    </Prop> */}
-
-   {/* <Welcome name="SHUWANI">
-      <p>This is my daughter</p>
-    </Welcome>
-    <Welcome name="SHUHASHI">
-      <button>Action</button>
-  </Welcome> */}
-    
-    {/* <States/>  */}
-
-    {/* <Setstate/> */}
-
-    {/* <Eventhandling/> */}
- 
-
     </div>
   );
 }
